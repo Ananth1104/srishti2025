6e4ee47d9a55c7655f6a4945293ede328b78a82d
Ananth1104 <121655639+Ananth1104@users.noreply.github.com>
2025-02-24 15:00:08 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,10 @@
-echo "gunicorn -w 4 -b 0.0.0.0:\$PORT app:app" > startup.sh
-chmod +x startup.sh
+#!/bin/bash
+
+# Set the number of Gunicorn workers (adjust based on expected load)
+WORKERS=4    # Number of worker processes
+THREADS=10   # Number of threads per worker
+
+# Start the Gunicorn server
+exec gunicorn -w $WORKERS --threads $THREADS -k gthread -b 0.0.0.0:$PORT app:app
+
+echo "running"
